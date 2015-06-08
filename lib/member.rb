a6d56c05f97e74a350975ab48366ad3091b3adf9
steven francisco <stvnfrancisco@yahoo.com>
2015-06-07 20:54:12 -0700

    add attr_reader to class Member and class Team

--- lib/member.rb
+++ lib/member.rb
@@ -1,18 +1,19 @@
 class Member
   @@members = []
+  attr_reader(:name, :id)
 
   define_method(:initialize) do |name|
     @name = name
     @id = @@members.length().+(1)
   end
 
-  define_method(:name) do
-    @name
-  end
-
-  define_method(:id) do
-   @id
-  end
+  # define_method(:name) do
+  #   @name
+  # end
+  #
+  # define_method(:id) do
+  #  @id
+  # end
 
   define_singleton_method(:all) do
     @@members
